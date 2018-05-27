dff24e8ec60ada644c1cff86d666883232171110
ddiazorz <ddiazorz@gmail.com>
2018-05-27 13:32:56 -0600

    demux 'terminado' favor revisar y preguntar cualquier cosa whatsapp

--- mux/generador_d_demux.v
+++ mux/generador_d_demux.v
@@ -13,9 +13,115 @@ module generador_d_demux(
     initial begin
         @(posedge clk);
         valid <= 1'b1;
+        ////////////////////////////////
         data <= COM;
         @(posedge clk);
-        
+        data <= COM;
+        @(posedge clk);
+        data <= COM;
+        @(posedge clk);
+        data <= COM;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= STP;
+        @(posedge clk);
+        data <= 8'h01;
+        @(posedge clk);
+        data <= 8'h02;
+        @(posedge clk);
+        data <= END;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= COM;
+        @(posedge clk);
+        data <= COM;
+        @(posedge clk);
+        data <= COM;
+        @(posedge clk);
+        data <= COM;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= SKP;
+        @(posedge clk);
+        data <= SKP;
+        @(posedge clk);
+        data <= SKP;
+        @(posedge clk);
+        data <= SKP;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= SKP;
+        @(posedge clk);
+        data <= SKP;
+        @(posedge clk);
+        data <= SKP;
+        @(posedge clk);
+        data <= SKP;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= SKP;
+        @(posedge clk);
+        data <= SKP;
+        @(posedge clk);
+        data <= SKP;
+        @(posedge clk);
+        data <= SKP;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= STP;
+        @(posedge clk);
+        data <= 8'h03;
+        @(posedge clk);
+        data <= 8'h04;
+        @(posedge clk);
+        data <= 8'h05;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= 8'h06;
+        @(posedge clk);
+        data <= 8'h07;
+        @(posedge clk);
+        data <= 8'h08;
+        @(posedge clk);
+        data <= 8'h09;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= 8'h0A;
+        @(posedge clk);
+        data <= 8'h0B;
+        @(posedge clk);
+        data <= 8'h0C;
+        @(posedge clk);
+        data <= END;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= SDP;
+        @(posedge clk);
+        data <= 8'h0D;
+        @(posedge clk);
+        data <= 8'h0E;
+        @(posedge clk);
+        data <= END;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= IDL;
+        @(posedge clk);
+        data <= IDL;
+        @(posedge clk);
+        data <= IDL;
+        @(posedge clk);
+        data <= IDL;
+        @(posedge clk);
+        ////////////////////////////////
+        data <= IDL;
+        @(posedge clk);
+        data <= IDL;
+        @(posedge clk);
+        data <= IDL;
+        @(posedge clk);
+        data <= IDL;
+        @(posedge clk);
+        ////////////////////////////////
 
     $finish;
     end
--- readme.txt
+++ readme.txt
@@ -1,1 +1,17 @@
 //--
+
+Para los relojs de las secciones del programa tomar en cuenta lo siguiente:
+
+Para poner el tiempo en escala de ns:
+
+`timescale 	1ns / 1ps
+
+
+Para frecuencia de 1MHz (mux y demux)
+#500 clk <= ~clk;
+
+Para frecuencia de 250kHz (bit striping y de-bit striping)
+#2000 clk <= ~clk;
+
+Para frecuencia de 2MHz ( serie-paralelo y paralelo-serie)
+#250 clk <= ~clk;
